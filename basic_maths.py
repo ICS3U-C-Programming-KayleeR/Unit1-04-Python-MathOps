ba8c27916bb3cb16ead3997a05dff250f9c63ba5
Kaylee Ralejoe <lintle.ralejoe@stu.ocsb.ca>
2026-09-16 13:11:38 +0000

    added basic_maths.py to cloud

---
+++ basic_maths.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Created By: Kaylee Ralejoe
+# Date: 15,09,2026
+# Testing basic math operations
+
+
+def main():
+    print("17-6 =", 17 - 6)
+    print("38-15={}".format(38 - 15))
+    print("3*11={}".format(3 * 11))
+    print("57/6={}".format(57 / 6))
+    print("213**0.5={}".format(213**0.5))
+
+
+if __name__ == "__main__":
+    main()
